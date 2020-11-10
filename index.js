6d16fe0e1c635cd0b01edc1c7e7ddf276b79f243
Ruben Baskaran <rbaskaran92@gmail.com>
2020-11-10 13:23:47 +0100

    added body parser

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+  console.log("Started on port 3000");
+});
+
+app.get("/", function (req, res) {
+  res.send("Hello there!");
+
+  //   let firstname = req.body.firstname;
+  //   let lastname = req.body.lastname;
+});
+
+app.get("*", function (req, res) {
+  res.send("This is the default route");
+});
