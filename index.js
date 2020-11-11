fed139d0ede684a3319231cd8adbe6d155daf8c0
Ruben Baskaran <rbaskaran92@gmail.com>
2020-11-11 14:56:58 +0100

    trying to call redcap api

--- index.js
+++ index.js
@@ -7,24 +7,23 @@ var FormData = require("form-data");
 var app = express();
 app.use(bodyParser.json());
 
-app.listen(3000, () => {
-  console.log("Started on port 3000");
+app.listen(3001, () => {
+  console.log("Started on port 3001");
 });
 
-app.post("/", function (req, res) {
-  res.send("Hello there!");
-  let firstname = req.body.firstname;
-  let lastname = req.body.lastname;
-  console.log("Fullname: " + firstname + " " + lastname);
+app.get("/", function (req, res) {
+  // res.send("Hello there!");
+  // let firstname = req.body.firstname;
+  // let lastname = req.body.lastname;
+  // console.log("Fullname: " + firstname + " " + lastname);
 
   ////////////////////
   // REDCAP REQUEST //
   ////////////////////
-  let url =
-    "https://cors-anywhere.herokuapp.com/https://open.rsyd.dk/redcap_uddannelse/api/"; // TODO: Remember to remove proxy in production
+  let url = "https://open.rsyd.dk/redcap_uddannelse/api/";
 
   let data = new FormData();
-  data.append("token", token);
+  data.append("token", "token");
   data.append("content", "record");
   data.append("format", "json");
   data.append("type", "flat");
@@ -32,23 +31,26 @@ app.post("/", function (req, res) {
   data.append("forceAutoNumber", "false");
   data.append(
     "data",
-    '[{"record_id":"3", "firstname":"TestName1", "lastname": "TestName2","age":"3","my_first_instrument_complete":"0" }]'
+    '[{"record_id":"3", "firstname":"TestName1", "lastname": "TestName2","age":"3","my_first_instrument_complete":"2" }]'
   );
   data.append("returnContent", "count");
   data.append("returnFormat", "json");
 
-  console.log("URL: " + url);
-  console.log("DATA: " + data);
+  let config = {
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "x-requested-with": "application/json",
+    },
+  };
 
   async function start() {
     try {
-      // let response = await axios.post(url, data);
-      let response = await axios.post("http://localhost:3000/feg"); // TODO: Try with above line instead
+      let response = await axios.post(url, data, config);
       console.log("RESPONSE:");
       console.log(response);
     } catch (err) {
       console.log("ERROR:");
-      console.log(err.message);
+      console.log(err);
     }
   }
   start();
