78ea0917207b4fe2771ecbc06ac4836d02e995e9
Ruben Baskaran <rbaskaran92@gmail.com>
2021-01-19 00:09:31 +0100

    added record_id check and commented test environment code

--- index.js
+++ index.js
@@ -8,29 +8,29 @@ var app = express();
 app.use(bodyParser.json());
 
 // Add headers (TODO: remove in production)
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
 
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
 
-  // Pass to next layer of middleware
-  next();
-});
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 const PORT = process.env.PORT || 3001;
 
@@ -45,6 +45,10 @@ app.listen(PORT, () => {
 // });
 
 app.post("/create", function (req, res) {
+  if (req.body.record_id !== "null") {
+    return;
+  }
+
   let data = new FormData();
   data.append("forceAutoNumber", "true");
   data.append("returnContent", "auto_ids");
@@ -52,7 +56,7 @@ app.post("/create", function (req, res) {
 });
 
 app.post("/update", function (req, res) {
-  if (req.body.record_id == "null") {
+  if (req.body.record_id === "null") {
     return;
   }
   let data = new FormData();
