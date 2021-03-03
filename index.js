615d473ddc3be830ac10d638070e093c885d0586
Ruben Baskaran <rbaskaran92@gmail.com>
2021-03-03 02:30:04 +0100

    added sdu reach url

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://respekt-frontend.herokuapp.com"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "https://reach.sdu.dk");
 
   // Request methods you wish to allow
   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
